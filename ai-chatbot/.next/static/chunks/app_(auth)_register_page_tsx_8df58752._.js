f739530844577b349f05b5a5ffa3d1fddc91be10
Bennguyenru <135408488+Bennguyenru@users.noreply.github.com>
2025-03-06 14:45:02 +0700

    Add ai-chatbot/.next/static/chunks/app_(auth)_register_page_tsx_8df58752._.js 1741247103655

---
+++ ai-chatbot/.next/static/chunks/app_(auth)_register_page_tsx_8df58752._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(auth)_register_page_tsx_8df58752._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(auth)_register_page_tsx_8df58752._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_a3ae6ac6._.js",
+    "static/chunks/_f2fa7e9b._.js",
+    "static/chunks/node_modules__pnpm_7da6266a._.js"
+  ],
+  "source": "dynamic"
+});
