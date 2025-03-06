45f7d22576fe0eb29780285938c5958cd8f6fceb
Bennguyenru <135408488+Bennguyenru@users.noreply.github.com>
2025-03-06 14:45:15 +0700

    Add ai-chatbot/.next/static/chunks/app_layout_tsx_ddb06626._.js 1741247116444

---
+++ ai-chatbot/.next/static/chunks/app_layout_tsx_ddb06626._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_ddb06626._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_ddb06626._.js",
+  "chunks": [
+    "static/chunks/_38610adf._.js",
+    "static/chunks/app_globals_73c37791.css"
+  ],
+  "source": "dynamic"
+});
